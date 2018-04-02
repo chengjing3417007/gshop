6f2c658ee8eadb191683dadc5b78759fc1114b9e
Cheng Jing <chengjing3417007@163.com>
2018-04-02 21:39:41 +0800

    improve the interface

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,13 @@
 </template>
 
 <script>
+import {reqAdress} from './api'
 import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 export default {
+  async mounted () {
+    const result = await reqAdress('40.10038,116.36867')
+    console.log(result)
+  },
   name: 'App',
   components: {
     FooterGuide
--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,31 +1,27 @@
 import axios from 'axios'
 
 export default function ajax (url = '', data = {}, type = 'GET') {
+  /* eslint-disable no-new */
   return new Promise(function (resolve, reject) {
     let promise
-
     if (type === 'GET') {
-      // 准备url query参数数据
-      let dataStr = '' // 数据拼接字符串
+      let dataStr = ''
+      // /login?name=xx&pwd=123
       Object.keys(data).forEach(key => {
         dataStr += key + '=' + data[key] + '&'
       })
-      if (dataStr !== '') {
-        dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'))
-        url = url + '?' + dataStr
+      if (dataStr) {
+        dataStr = dataStr.substr(0, dataStr.length - 1)
       }
-      // 发送get请求
+      url += '?' + dataStr
       promise = axios.get(url)
     } else {
-      // 发送post请求
       promise = axios.post(url, data)
     }
-
     promise.then(response => {
       resolve(response.data)
+    }).catch(err => {
+      reject(err)
     })
-      .catch(error => {
-        reject(error)
-      })
   })
 }
